6da64e470b3ebc84769ba4a1d87363d114d920d8
gslooters <gslooters@gslmcc.net>
2025-11-02 10:39:43 -0400

    fix: vervang getRosters door readRosters (of compat wrapper) na storage.ts refactor

--- app/planning/libAliases.ts
+++ app/planning/libAliases.ts
@@ -1,3 +1,6 @@
-export { getRosters } from '../../lib/planning/storage';
 export { isDutchHoliday } from '../../lib/planning/holidays';
 export { isAvailable } from '../../lib/planning/availability';
+export { upsertRoster, computeDefaultStart, validateStartMonday, computeEnd } from '../../lib/planning/storage';
+
+// Compat: oude naam 'getRosters' als wrapper om readRosters
+export { readRosters as getRosters } from '../../lib/planning/storage';
