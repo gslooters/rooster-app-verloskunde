4c759ca45536dc976ca33439dccfcb3912c47b0d
gslooters <gslooters@gslmcc.net>
2025-11-02 12:40:45 -0400

    Fix CSS import to relative path '../toolbar.css' for Next.js app dir

--- app/planning/PlanningGrid.tsx
+++ app/planning/PlanningGrid.tsx
@@ -4,7 +4,7 @@ import React, { useEffect, useMemo, useState } from 'react';
 import { getRosters, isDutchHoliday, isAvailable } from './libAliases';
 import AvailabilityPopup from './_components/AvailabilityPopup';
 import '@/styles/planning.css';
-import '@/toolbar.css';
+import '../toolbar.css';
 
 import { prepareRosterForExport, exportToExcel, exportToCSV, exportRosterToPDF, exportEmployeeToPDF } from '@/lib/export';
 
