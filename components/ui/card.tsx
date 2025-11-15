40f06ba7af50a563dd24a61f807f2f20fe23de25
Govard Slooters <gslooters@gslmcc.net>
2025-11-15 16:36:39 -0400

    Add Card UI component

---
+++ components/ui/card.tsx
@@ -0,0 +1,23 @@
+import * as React from 'react';
+
+interface CardProps extends React.HTMLAttributes<HTMLDivElement> {
+  children: React.ReactNode;
+}
+
+const Card = React.forwardRef<HTMLDivElement, CardProps>(
+  ({ className = '', children, ...props }, ref) => {
+    return (
+      <div
+        ref={ref}
+        className={`bg-white rounded-lg border border-gray-200 shadow-sm ${className}`}
+        {...props}
+      >
+        {children}
+      </div>
+    );
+  }
+);
+
+Card.displayName = 'Card';
+
+export { Card };
