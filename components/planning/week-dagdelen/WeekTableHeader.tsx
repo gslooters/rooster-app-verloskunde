5ca89bb57ce222e6a736024cfea0c9b98907fdb6
Govard Slooters <gslooters@gslmcc.net>
2025-11-21 09:26:07 -0400

    fix: Correcte header layout - dagnamen BOVEN emoji's (DRAAD40B5)

--- components/planning/week-dagdelen/WeekTableHeader.tsx
+++ components/planning/week-dagdelen/WeekTableHeader.tsx
@@ -27,20 +27,42 @@ export function WeekTableHeader({ weekDagen }: WeekTableHeaderProps) {
 
   return (
     <thead className="sticky top-[64px] z-30 bg-blue-50">
-      {/* Row 1: Dienst + Team + Dagdeel emoji's horizontaal */}
+      {/* Row 1: Dienst + Team (rowSpan=2) + Dag namen BOVEN emoji's */}
       <tr>
         <th 
+          rowSpan={2}
           className="frozen-left-1 text-center font-semibold text-gray-700 border border-gray-300 p-2 min-w-[120px] bg-white"
           style={{ position: 'sticky', left: 0, zIndex: 35 }}
         >
           Dienst
         </th>
         <th 
+          rowSpan={2}
           className="frozen-left-2 text-center font-semibold text-gray-700 border border-gray-300 p-2 min-w-[100px] bg-white"
           style={{ position: 'sticky', left: '120px', zIndex: 35 }}
         >
           Team
         </th>
+        {weekDagen.map((dag) => (
+          <th
+            key={dag.datum}
+            colSpan={3}
+            className="text-center border-l-2 border-gray-400 border-t border-r border-gray-300 p-1 bg-blue-100"
+          >
+            <div className="flex flex-col items-center gap-0">
+              <span className="font-bold text-xs text-gray-800 uppercase">
+                {dag.dagSoort}
+              </span>
+              <span className="text-[10px] text-gray-600 font-medium">
+                {formatDatum(dag.datum)}
+              </span>
+            </div>
+          </th>
+        ))}
+      </tr>
+      
+      {/* Row 2: Emoji's ONDER de dag namen (Dienst/Team hebben rowSpan dus geen cellen hier) */}
+      <tr>
         {weekDagen.map((dag) => (
           <React.Fragment key={`dagdelen-${dag.datum}`}>
             <th className="dagdeel-header text-center border border-gray-300 p-0.5 bg-orange-50 min-w-[40px]">
@@ -59,36 +81,6 @@ export function WeekTableHeader({ weekDagen }: WeekTableHeaderProps) {
               </span>
             </th>
           </React.Fragment>
-        ))}
-      </tr>
-      
-      {/* Row 2: Dag namen + datums onder de dagdelen */}
-      <tr>
-        <th 
-          className="border border-gray-300 bg-white"
-          style={{ position: 'sticky', left: 0, zIndex: 35 }}
-        >
-        </th>
-        <th 
-          className="border border-gray-300 bg-white"
-          style={{ position: 'sticky', left: '120px', zIndex: 35 }}
-        >
-        </th>
-        {weekDagen.map((dag) => (
-          <th
-            key={dag.datum}
-            colSpan={3}
-            className="text-center border-l-2 border-gray-400 border-t border-r border-gray-300 p-1 bg-blue-100"
-          >
-            <div className="flex flex-col items-center gap-0">
-              <span className="font-bold text-xs text-gray-800 uppercase">
-                {dag.dagSoort}
-              </span>
-              <span className="text-[10px] text-gray-600 font-medium">
-                {formatDatum(dag.datum)}
-              </span>
-            </div>
-          </th>
         ))}
       </tr>
     </thead>
