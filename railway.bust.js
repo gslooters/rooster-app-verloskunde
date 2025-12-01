2b46e9d7f16abd91122bb57717936e16aab2cb36
Govard Slooters <gslooters@gslmcc.net>
2025-12-02 00:02:54 +0100

    DRAAD95D: Cache-bust voor Railway deployment
    Trigger nieuwe deployment met RosterPlanningRulesModal UI

--- railway.bust.js
+++ railway.bust.js
@@ -1,6 +1,6 @@
 // Railway triggerbestand voor cache-bust
 // Elke deployment vereist nieuwe random string in bust value
-// DRAAD95C - Database Table Name Fix
+// DRAAD95D - Rooster-specifieke Planregels UI Implementatie
 module.exports = {
-  bust: `1733092800000-${Math.floor(Math.random() * 100000)}` // DRAAD95C - Dec 1, 2025 23:20 CET
+  bust: `1733097745000-${Math.floor(Math.random() * 100000)}` // DRAAD95D - Dec 2, 2025 00:02 CET
 };
