cd8553f3e37090446a7b33e160b53b31873e83dc
Govard Slooters <gslooters@gslmcc.net>
2025-12-02 00:19:24 +0100

    DRAAD95E: Cache-bust voor bugfix deployment
    Column name fix rosterid -> roster_id

--- railway.bust.js
+++ railway.bust.js
@@ -1,6 +1,6 @@
 // Railway triggerbestand voor cache-bust
 // Elke deployment vereist nieuwe random string in bust value
-// DRAAD95D - Rooster-specifieke Planregels UI Implementatie
+// DRAAD95E - Column Name Bugfix rosterid -> roster_id
 module.exports = {
-  bust: `1733097745000-${Math.floor(Math.random() * 100000)}` // DRAAD95D - Dec 2, 2025 00:02 CET
+  bust: `1733098748000-${Math.floor(Math.random() * 100000)}` // DRAAD95E - Dec 2, 2025 00:19 CET
 };
