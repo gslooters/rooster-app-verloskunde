a60614fd5278670c1b4e5d56a791c427cf67ceb4
Govard Slooters <gslooters@gslmcc.net>
2025-12-21 20:43:44 +0100

    fix: remove obsolete app/planning/libAliases.ts

--- app/planning/libAliases.ts
+++
@@ -1,6 +0,0 @@
-export { isDutchHoliday } from '../../lib/planning/holidays';
-export { isAvailable } from '../../lib/planning/availability';
-export { upsertRoster, computeDefaultStart, validateStartMonday, computeEnd } from '../../lib/planning/storage';
-
-// Compat: oude naam 'getRosters' als wrapper om readRosters
-export { readRosters as getRosters } from '../../lib/planning/storage';
