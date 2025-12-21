bee1205335ae08195045ccb8d658981feef48da0
Govard Slooters <gslooters@gslmcc.net>
2025-12-21 20:43:56 +0100

    fix: remove obsolete app/planning/[id]/page.tsx

--- app/planning/[id]/page.tsx
+++
@@ -1,8 +0,0 @@
-import PlanningGrid from '../PlanningGrid';
-
-export const dynamic = 'force-dynamic';
-
-export default function RosterDetail({ params }: { params: { id: string } }) {
-  return <PlanningGrid rosterId={params.id} />;
-}
-
