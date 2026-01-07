24dede4ca5d27239cad81b5c99967eceb834a516
Govard Slooters <gslooters@gslmcc.net>
2026-01-07 16:29:33 +0100

    DRAAD406F3: Export AflReportModal uit index.ts

--- components/afl/index.ts
+++ components/afl/index.ts
@@ -2,6 +2,8 @@
  * AFL (AutoFill) Components
  * 
  * DRAAD335: Frontend components for AFL pipeline integration
+ * DRAAD406F3: Added AflReportModal for PDF-friendly rapport weergave
  */
 
 export { AflProgressModal } from './AflProgressModal';
+export { AflReportModal } from './AflReportModal';
